745084513f4a7234a4be1125642c32c75379642e
Owen Grant <owen.dalmon.grant@gmail.com>
2020-05-04 20:45:46 -0400

    add utils class with resources contants

---
+++ src/main/java/owen/game/tank/entities/EntityUtils.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package owen.game.tank.entities;
+
+/**
+ *
+ * @author owen
+ */
+public class EntityUtils {
+    public final static String PLAYER1_TANK4 = "player1_tank4";
+}
