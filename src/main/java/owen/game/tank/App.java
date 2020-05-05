78a081cf6430714e910316da92fa801e9e1c8ac8
amasurya <layne.alicia.a@gmail.com>
2020-05-04 20:36:16 -0400

    add fxgl

--- src/main/java/owen/game/tank/App.java
+++ src/main/java/owen/game/tank/App.java
@@ -1,29 +1,110 @@
 package owen.game.tank;
 
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import java.util.Map;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
  * JavaFX App
  */
-public class App extends Application {
+public class App extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(600);
+        settings.setHeight(600);
+        settings.setTitle("Basic Game App");
+        settings.setVersion("0.1");
+    }
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+    protected void initInput() {
+        Input input = FXGL.getInput();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        input.addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                player.translateX(5); // move right 5 pixels
+                FXGL.getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.D);
+
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5); // move left 5 pixels
+                FXGL.getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.A);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5); // move up 5 pixels
+                FXGL.getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.W);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5); // move down 5 pixels
+                FXGL.getGameState().increment("pixelsMoved", +5);
+            }
+        }, KeyCode.S);
+
+        input.addAction(new UserAction("Play Sound") {
+            @Override
+            protected void onActionBegin() {
+                FXGL.play("drop.wav");
+            }
+        }, KeyCode.F);
+    }
+
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
+    }
+
+    private Entity player;
+
+    @Override
+    protected void initGame() {
+        player = FXGL.entityBuilder()
+                .at(300, 300)
+                //.view(new Rectangle(25, 25, Color.BLUE))
+                .view("brick.png")
+                .buildAndAttach();
+    }
+
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50); // x = 50
+        textPixels.setTranslateY(100); // y = 100
+
+        textPixels.textProperty().bind(FXGL.getGameState().intProperty("pixelsMoved").asString());
+
+        FXGL.getGameScene().addUINode(textPixels); // add to the scene graph
+
+        var brickTexture = FXGL.getAssetLoader().loadTexture("brick.png");
+        brickTexture.setTranslateX(50);
+        brickTexture.setTranslateY(450);
+
+        FXGL.getGameScene().addUINode(brickTexture);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
